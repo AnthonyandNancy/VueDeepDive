84d89abfb1d6ac6dd6be0b019297021d443cf16e
郭建林 <1050070285@qq.com>
2020-11-07 16:18:27 +0800

    vue源码-observer

---
+++ js/observer.js
@@ -0,0 +1,32 @@
+class Observer {
+    constructor(data) {
+        this.walk(data)
+    }
+    walk(data){
+       //1`判断是否是对象
+        if ( !data ||typeof data !=='object'){return}
+       //2`遍历对象
+        Object.keys(data).forEach(key=>{
+            this.defineReactive(data,key,data[key])
+        })
+    }
+    defineReactive(obj,key,val){
+        var that=this
+        //当data里面的数据是对象的时候,并没有getter和setter
+        this.walk(val)
+        Object.defineProperty(obj,key,{
+            enumerable:true,
+            configurable:true,
+            get() {
+                return val
+            },
+            set(newValue) {
+                if (newValue ===val){return}
+                val=newValue
+                //当用户重新设置值的时候,并没有getter和setter
+                that.walk(newValue)
+                //发送通知
+            }
+        })
+    }
+}
